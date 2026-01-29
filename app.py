adc72276d4835112b88e8a0202bbee78c2b5316b
Carlos Tapia <carlostapia@Carloss-MacBook-Air.local>
2026-01-28 20:42:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,25 +2,25 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+st.set_page_config(page_title="Dashboard de anuncios de coches", layout="centered")
+
 st.header("Dashboard de anuncios de coches")
-
-car_data = pd.read_csv("vehicles_us.csv")
-
 st.write("Vista rápida del dataset:")
-st.dataframe(car_data.head())
+
+df = pd.read_csv("vehicles_us.csv")
+st.dataframe(df.head(10), use_container_width=True)
 
 hist_button = st.button("Construir histograma (odometer)")
 
 if hist_button:
     st.write("Histograma del odómetro")
-    fig_hist = px.histogram(car_data, x="odometer")
+    fig_hist = px.histogram(df, x="odometer", nbins=50)
     st.plotly_chart(fig_hist, use_container_width=True)
 
 scatter_button = st.button("Construir dispersión (odometer vs price)")
 
 if scatter_button:
-    st.write("Dispersión: odometer vs price")
-    fig_scatter = px.scatter(car_data, x="odometer", y="price")
+    st.write("Relación entre odómetro y precio")
+    fig_scatter = px.scatter(df, x="odometer", y="price")
     st.plotly_chart(fig_scatter, use_container_width=True)
-
 
